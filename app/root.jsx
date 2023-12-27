a09632693c7b2d0d6e0aedffab95fcc0f05aa9cf
Justin Andrews <justin.48@hotmail.com>
2023-12-26 19:51:27 -0700

    updated html

--- app/root.jsx
+++ app/root.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h1>Hello world!</h1>
+        <h1>Hello William Evans!! Want to play a game?</h1>
         <Outlet />
 
         <Scripts />
