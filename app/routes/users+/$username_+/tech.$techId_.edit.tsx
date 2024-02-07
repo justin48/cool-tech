7e60bb1989c3fcd531534bdf9e1f8af75248b882
Justin Andrews <justin.48@hotmail.com>
2024-02-07 09:33:10 -0700

    started form validation

--- app/routes/users+/$username_+/tech.$techId_.edit.tsx
+++ app/routes/users+/$username_+/tech.$techId_.edit.tsx
@@ -1,13 +1,14 @@
 import {
   Form,
+  useActionData,
   useFormAction,
   useLoaderData,
   useNavigation,
 } from "@remix-run/react";
 import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
-import { db } from "#app/utils/db.server.ts";
-import { invariantResponse } from "#app/utils/misc.tsx";
-import React from "react";
+import { db, updateTech } from "#app/utils/db.server.ts";
+import { invariantResponse, useIsSubmitting } from "#app/utils/misc.tsx";
+import React, { useEffect, useState } from "react";
 import { floatingToolbarClassName } from "#app/components/floating-toolbar.tsx";
 import { Button } from "#app/components/ui/button.tsx";
 import { Input } from "#app/components/ui/input.tsx";
@@ -15,6 +16,7 @@ import { Label } from "#app/components/ui/label.tsx";
 import { Textarea } from "#app/components/ui/textarea.tsx";
 import { StatusButton } from "#app/components/ui/status-button.tsx";
 import { GeneralErrorBoundary } from "#app/components/error-boundary.tsx";
+import { err } from "#node_modules/@remix-run/dev/dist/result.js";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const tech = db.tech.findFirst({
@@ -32,6 +34,17 @@ export async function loader({ params }: LoaderFunctionArgs) {
   });
 }
 
+type ActionErrors = {
+  formErrors: Array<string>;
+  fieldErrors: {
+    title: Array<string>;
+    content: Array<string>;
+  };
+};
+
+const titleMaxLength = 100;
+const contentMaxLength = 10000;
+
 export async function action({ request, params }: LoaderFunctionArgs) {
   const formData = await request.formData();
   const title = formData.get("title");
@@ -39,36 +52,104 @@ export async function action({ request, params }: LoaderFunctionArgs) {
   invariantResponse(typeof title === "string", "Title must be a string");
   invariantResponse(typeof content === "string", "Content must be a string");
 
-  db.tech.update({
-    where: { id: { equals: params.techId } },
-    data: { title, content },
-  });
+  const errors: ActionErrors = {
+    formErrors: [],
+    fieldErrors: {
+      title: [],
+      content: [],
+    },
+  };
+
+  if (title === "") {
+    errors.fieldErrors.title.push("Title is required");
+  }
+  if (title.length > titleMaxLength) {
+    errors.fieldErrors.title.push(
+      `Title is too large. Must be ${titleMaxLength} characters or less`,
+    );
+  }
+  if (content === "") {
+    errors.fieldErrors.content.push("Content is required");
+  }
+  if (content.length > contentMaxLength) {
+    errors.fieldErrors.content.push(
+      `Content is too large. Must be ${contentMaxLength} characters or less`,
+    );
+  }
+
+  const hasErrors =
+    errors.formErrors.length ||
+    Object.values(errors.fieldErrors).some((fieldErrors) => fieldErrors.length);
+  if (hasErrors) {
+    return json({ status: "error", errors } as const, { status: 400 });
+  }
+
+  await updateTech({ id: params.techId, title, content });
 
   return redirect(`/users/${params.username}/tech/${params.techId}`);
 }
 
+function ErrorList({ errors }: { errors?: Array<string> | null }) {
+  return errors?.length ? (
+    <ul className="flex flex-col gap-1">
+      {errors.map((error, i) => (
+        <li key={i} className="text-[10px] text-foreground-destructive">
+          {error}
+        </li>
+      ))}
+    </ul>
+  ) : null;
+}
+
+function useHydrated() {
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => setHydrated(true), []);
+  return hydrated;
+}
+
 export default function TechEdit() {
   const data = useLoaderData<typeof loader>();
-  const navigation = useNavigation();
-  const formAction = useFormAction();
-  const isSubmitting =
-    navigation.state !== "idle" &&
-    navigation.formMethod === "POST" &&
-    navigation.formAction === formAction;
+  const actionData = useActionData<typeof action>();
+  const isSubmitting = useIsSubmitting();
+  const formId = "note-editor";
+
+  const fieldErrors =
+    actionData?.status === "error" ? actionData.errors.fieldErrors : null;
+  const formErrors =
+    actionData?.status === "error" ? actionData.errors.formErrors : null;
+  const isHydrated = useHydrated();
 
   return (
     <Form
+      id={formId}
       method="POST"
+      noValidate={isHydrated}
       className="flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12"
     >
       <div className="flex flex-col gap-1">
         <div>
           <Label>Title</Label>
-          <Input name="title" defaultValue={data.tech.title} />
+          <Input
+            name="title"
+            defaultValue={data.tech.title}
+            required
+            maxLength={titleMaxLength}
+          />
+          <div className="min-h-[32px] px-4 pb-3 pt-1">
+            <ErrorList errors={fieldErrors?.title} />
+          </div>
         </div>
         <div>
           <Label>Content</Label>
-          <Textarea name="content" defaultValue={data.tech.content} />
+          <Textarea
+            name="content"
+            defaultValue={data.tech.content}
+            required
+            maxLength={contentMaxLength}
+          />
+          <div className="min-h-[32px] px-4 pb-3 pt-1">
+            <ErrorList errors={fieldErrors?.content} />
+          </div>
         </div>
       </div>
       <div className={floatingToolbarClassName}>
@@ -76,6 +157,7 @@ export default function TechEdit() {
           Reset
         </Button>
         <StatusButton
+          form={formId}
           type="submit"
           disabled={isSubmitting}
           status={isSubmitting ? "pending" : "idle"}
@@ -83,6 +165,7 @@ export default function TechEdit() {
           Submit
         </StatusButton>
       </div>
+      <ErrorList errors={formErrors} />
     </Form>
   );
 }
