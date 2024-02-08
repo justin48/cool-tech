07e83b61288de753ddb38214dd10a4d2378e0e7b
Justin Andrews <justin.48@hotmail.com>
2024-02-07 18:43:16 -0700

    added image upload

--- app/routes/users+/$username_+/tech.$techId_.edit.tsx
+++ app/routes/users+/$username_+/tech.$techId_.edit.tsx
@@ -1,14 +1,13 @@
+import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import {
-  Form,
-  useActionData,
-  useFormAction,
-  useLoaderData,
-  useNavigation,
-} from "@remix-run/react";
-import { json, LoaderFunctionArgs, redirect } from "@remix-run/node";
+  json,
+  LoaderFunctionArgs,
+  redirect,
+  unstable_parseMultipartFormData as parseMultipartFormData,
+} from "@remix-run/node";
 import { db, updateTech } from "#app/utils/db.server.ts";
-import { invariantResponse, useIsSubmitting } from "#app/utils/misc.tsx";
-import React, { useEffect, useId, useRef, useState } from "react";
+import { cn, invariantResponse, useIsSubmitting } from "#app/utils/misc.tsx";
+import React, { useState } from "react";
 import { floatingToolbarClassName } from "#app/components/floating-toolbar.tsx";
 import { Button } from "#app/components/ui/button.tsx";
 import { Input } from "#app/components/ui/input.tsx";
@@ -16,10 +15,10 @@ import { Label } from "#app/components/ui/label.tsx";
 import { Textarea } from "#app/components/ui/textarea.tsx";
 import { StatusButton } from "#app/components/ui/status-button.tsx";
 import { GeneralErrorBoundary } from "#app/components/error-boundary.tsx";
-import { err } from "#node_modules/@remix-run/dev/dist/result.js";
-import { z } from "zod";
+import { optional, z } from "zod";
 import { conform, useForm } from "@conform-to/react";
 import { getFieldsetConstraint, parse } from "@conform-to/zod";
+import { createMemoryUploadHandler } from "#node_modules/@remix-run/server-runtime/dist/upload/memoryUploadHandler.js";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const tech = db.tech.findFirst({
@@ -33,12 +32,18 @@ export async function loader({ params }: LoaderFunctionArgs) {
   invariantResponse(tech, "Tech not found", { status: 404 });
 
   return json({
-    tech: { title: tech.title, content: tech.content },
+    tech: {
+      title: tech.title,
+      content: tech.content,
+      images: tech.images.map((i) => ({ id: i.id, altText: i.altText })),
+    },
   });
 }
 
 const titleMaxLength = 100;
 const contentMaxLength = 10000;
+
+const MAX_UPLOAD_SIZE = 1024 * 1024 * 3;
 
 const TechEditorSchema = z.object({
   title: z
@@ -49,12 +54,23 @@ const TechEditorSchema = z.object({
     .string()
     .min(1, { message: "Content is required" })
     .max(contentMaxLength),
+  imageId: z.string().optional(),
+  file: z
+    .instanceof(File)
+    .refine((file) => {
+      return file.size <= MAX_UPLOAD_SIZE;
+    }, "File size must be less than 3MB")
+    .optional(),
+  altText: z.string().optional(),
 });
 
 export async function action({ request, params }: LoaderFunctionArgs) {
   invariantResponse(params.techId, "techId param is required");
 
-  const formData = await request.formData();
+  const formData = await parseMultipartFormData(
+    request,
+    createMemoryUploadHandler({ maxPartSize: MAX_UPLOAD_SIZE }),
+  );
 
   const submission = parse(formData, {
     schema: TechEditorSchema,
@@ -64,9 +80,20 @@ export async function action({ request, params }: LoaderFunctionArgs) {
     return json({ status: "error", submission } as const, { status: 400 });
   }
 
-  const { title, content } = submission.value;
+  const { title, content, file, imageId, altText } = submission.value;
 
-  await updateTech({ id: params.techId, title, content });
+  await updateTech({
+    id: params.techId,
+    title,
+    content,
+    images: [
+      {
+        file,
+        id: imageId,
+        altText,
+      },
+    ],
+  });
 
   return redirect(`/users/${params.username}/tech/${params.techId}`);
 }
@@ -94,14 +121,6 @@ export default function TechEdit() {
   const actionData = useActionData<typeof action>();
   const isSubmitting = useIsSubmitting();
 
-  const titleId = useId();
-  const contentId = useId();
-
-  const fieldErrors =
-    actionData?.status === "error" ? actionData.submission.error : null;
-  const formErrors =
-    actionData?.status === "error" ? actionData.submission.error[""] : null;
-
   const [form, fields] = useForm({
     id: "tech-editor",
     constraint: getFieldsetConstraint(TechEditorSchema),
@@ -120,6 +139,7 @@ export default function TechEdit() {
       method="POST"
       className="flex h-full flex-col gap-y-4 overflow-x-hidden px-10 pb-28 pt-12"
       {...form.props}
+      encType="multipart/form-data"
     >
       <div className="flex flex-col gap-1">
         <div>
@@ -139,6 +159,10 @@ export default function TechEdit() {
             />
           </div>
         </div>
+        <div>
+          <Label>Image</Label>
+          <ImageChooser image={data.tech.images[0]} />
+        </div>
       </div>
       <div className={floatingToolbarClassName}>
         <Button form={form.id} variant="destructive" type="reset">
@@ -155,6 +179,89 @@ export default function TechEdit() {
       </div>
       <ErrorList id={form.id} errors={form.errors} />
     </Form>
+  );
+}
+
+function ImageChooser({
+  image,
+}: {
+  image?: { id: string; altText?: string | null };
+}) {
+  const existingImage = Boolean(image);
+  const [previewImage, setPreviewImage] = useState<string | null>(
+    existingImage ? `/resources/images/${image?.id}` : null,
+  );
+  const [altText, setAltText] = useState(image?.altText ?? "");
+
+  return (
+    <fieldset>
+      <div className="flex gap-3">
+        <div className="w-32">
+          <div className="relative h-32 w-32">
+            <label
+              htmlFor="image-input"
+              className={cn("group absolute h-32 w-32 rounded-lg", {
+                "bg-accent opacity-40 focus-within:opacity-100 hover:opacity-100":
+                  !previewImage,
+                "cursor-pointer focus-within:ring-4": !existingImage,
+              })}
+            >
+              {previewImage ? (
+                <div className="relative">
+                  <img
+                    src={previewImage}
+                    alt={altText ?? ""}
+                    className="h-32 w-32 rounded-lg object-cover"
+                  />
+                  {existingImage ? null : (
+                    <div className="pointer-events-none absolute -right-0.5 -top-0.5 rotate-12 rounded-sm bg-secondary px-2 py-1 text-xs text-secondary-foreground shadow-md">
+                      new
+                    </div>
+                  )}
+                </div>
+              ) : (
+                <div className="flex h-32 w-32 items-center justify-center rounded-lg border border-muted-foreground text-4xl text-muted-foreground">
+                  ➕
+                </div>
+              )}
+              {existingImage ? (
+                <input name="imageId" type="hidden" value={image?.id} />
+              ) : null}
+              <input
+                id="image-input"
+                aria-label="Image"
+                className="absolute left-0 top-0 z-0 h-32 w-32 cursor-pointer opacity-0"
+                onChange={(event) => {
+                  const file = event.target.files?.[0];
+
+                  if (file) {
+                    const reader = new FileReader();
+                    reader.onloadend = () => {
+                      setPreviewImage(reader.result as string);
+                    };
+                    reader.readAsDataURL(file);
+                  } else {
+                    setPreviewImage(null);
+                  }
+                }}
+                name="file"
+                type="file"
+                accept="image/*"
+              />
+            </label>
+          </div>
+        </div>
+        <div className="flex-1">
+          <Label htmlFor="alt-text">Alt Text</Label>
+          <Textarea
+            id="alt-text"
+            name="altText"
+            defaultValue={altText}
+            onChange={(e) => setAltText(e.currentTarget.value)}
+          />
+        </div>
+      </div>
+    </fieldset>
   );
 }
 
