f1ab0c8f90abde6b060ee6410626cb98df8cdf70
Justin Andrews <justin.48@hotmail.com>
2024-02-17 16:27:48 -0700

    got dark mode working

--- app/root.tsx
+++ app/root.tsx
@@ -1,130 +1,246 @@
 import os from "node:os";
 import {
-  Link,
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-  MetaFunction,
+    Link,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
+    MetaFunction,
+    useMatches,
+    useFetcher,
 } from "@remix-run/react";
-import { json, type LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import {
+    json,
+    type LinksFunction,
+    LoaderFunctionArgs,
+} from "@remix-run/node";
+import { SearchBar } from "./components/search-bar.tsx";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import { Button } from "./components/ui/button.tsx";
 import { AuthenticityTokenProvider } from "remix-utils/csrf/react";
+import { parse } from "@conform-to/zod";
 import { HoneypotProvider } from "remix-utils/honeypot/react";
 import faviconAssetUrl from "./assets/favicon.svg";
+import { ErrorList } from "./components/forms.tsx";
+import { z } from "zod";
 import tailwindStyleSheetUrl from "./styles/tailwind.css";
 import { getEnv } from "#app/utils/env.server.ts";
 import { GeneralErrorBoundary } from "#app/components/error-boundary.tsx";
 import { honeypot } from "./utils/honeypot.server.ts";
 import React from "react";
 import { csrf } from "./utils/csrf.server.ts";
+import { invariantResponse } from "#app/utils/misc.tsx";
+import { getTheme, setTheme, type Theme } from "./utils/theme.server.ts";
+import { useForm } from "@conform-to/react";
+import { Icon } from "#app/components/ui/icon.tsx";
+import { Spacer } from "#app/components/spacer.tsx";
 
 export const links: LinksFunction = () => {
-  return [
-    { rel: "icon", type: "image/svg+xml", href: faviconAssetUrl },
-    { rel: "stylesheet", href: tailwindStyleSheetUrl },
-  ];
-};
+    return [
+        {rel: 'icon', type: 'image/svg+xml', href: faviconAssetUrl},
+        {rel: 'stylesheet', href: tailwindStyleSheetUrl},
+        cssBundleHref ? {rel: 'stylesheet', href: cssBundleHref} : null,
+    ].filter(Boolean)
+}
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const honeyProps = honeypot.getInputProps();
-  const [csrfToken, csrfCookieHeader] = await csrf.commitToken(request);
-  return json(
-    { username: os.userInfo().username, ENV: getEnv(), honeyProps, csrfToken },
-    {
-      headers: csrfCookieHeader
-        ? {
-            "set-cookie": csrfCookieHeader,
-          }
-        : {},
-    },
-  );
+export async function loader({request}: LoaderFunctionArgs) {
+    const honeyProps = honeypot.getInputProps();
+    const [csrfToken, csrfCookieHeader] = await csrf.commitToken(request);
+    return json(
+        {username: os.userInfo().username, theme: getTheme(request), ENV: getEnv(), honeyProps, csrfToken},
+        {
+            headers: csrfCookieHeader
+                ? {
+                    "set-cookie": csrfCookieHeader,
+                }
+                : {},
+        },
+    );
+}
+
+const ThemeFormSchema = z.object({
+    theme: z.enum(["light", "dark"]),
+});
+
+export async function action({request}: LoaderFunctionArgs) {
+    const formData = await request.formData();
+    invariantResponse(
+        formData.get("intent") === "update-theme",
+        "Invalid intent",
+        {status: 400},
+    );
+
+    const submission = parse(formData, {
+        schema: ThemeFormSchema,
+    });
+    if (submission.intent !== "submit") {
+        return json({status: "success", submission} as const);
+    }
+    if (!submission.value) {
+        return json({status: "error", submission} as const, {status: 400});
+    }
+
+    const { theme } = submission.value
+
+    const responseInit = {
+        headers: { 'set-cookie': setTheme(theme)}
+    }
+
+    return json({success: true, submission}, responseInit);
+}
+
+export function Document({
+                             children,
+                             theme,
+                             env,
+                         }: {
+    children: React.ReactNode;
+    theme?: Theme;
+    env?: Record<string, string>;
+}) {
+    return (
+        <html lang="en" className={`${theme} h-full overflow-x-hidden`}>
+        <head>
+            <Meta/>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width,initial-scale=1"/>
+            <Links/>
+        </head>
+        <body className="flex h-full flex-col justify-between bg-background text-foreground">
+        {children}
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `window.ENV = ${JSON.stringify(env)}`,
+            }}
+        />
+        <ScrollRestoration/>
+        <LiveReload/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
 
 export function App() {
-  const data = useLoaderData<typeof loader>();
+    const data = useLoaderData<typeof loader>()
+	const theme = data.theme
+    const matches = useMatches();
+    const isOnSearchPage = matches.find((m) => m.id === "routes/users+/index");
 
-  return (
-    <Document>
-      <header className="container mx-auto py-6">
-        <nav className="flex justify-between">
-          <Link to="/">
-            <div className="font-light">Epic</div>
-            <div className="font-bold">Technologies!!</div>
-          </Link>
-          <Link className="underline" to="/signup">
-            Sign Up
-          </Link>
-        </nav>
-      </header>
-      <div className="flex-1">
-        <Outlet />
-      </div>
+    return (
+        <Document theme={theme} env={data.ENV}>
+            <header className="container px-6 py-4 sm:px-8 sm:py-6">
+                <nav className="flex items-center justify-between gap-4 sm:gap-6">
+                    <Link to="/">
+                        <div className="font-light">Epic</div>
+                        <div className="font-bold">Technologies!!</div>
+                    </Link>
+                    {isOnSearchPage ? null : (
+                        <div className="ml-auto max-w-sm flex-1">
+                            <SearchBar status="idle"/>
+                        </div>
+                    )}
+                    <div className="flex items-center gap-10">
+                        <Button asChild variant="default" size="sm">
+                            <Link to="/login">Log In</Link>
+                        </Button>
+                    </div>
+                </nav>
+            </header>
+            <div className="flex-1">
+                <Outlet/>
+            </div>
 
-      <div className="container mx-auto flex justify-between">
-        <Link to="/">
-          <div className="font-light">Epic</div>
-          <div className="font-bold">Technologies</div>
-        </Link>
-        <p>Built with ⚡️ by {data.username}</p>
-      </div>
-      <div className="h-5" />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.ENV = ${JSON.stringify(data.ENV)}`,
-        }}
-      />
-    </Document>
-  );
-}
-
-export function Document({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en" className="h-full overflow-x-hidden">
-      <head>
-        <Meta />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Links />
-      </head>
-      <body className="flex h-full flex-col justify-between bg-background text-foreground">
-        {children}
-        <ScrollRestoration />
-        <LiveReload />
-        <Scripts />
-      </body>
-    </html>
-  );
+            <div className="container flex justify-between">
+                <Link to="/">
+                    <div className="font-light">Epic</div>
+                    <div className="font-bold">Technologies</div>
+                </Link>
+                <div className="flex items-center gap-2">
+                    <p>Built with ⚡️ by {data.username}</p>
+                    <ThemeSwitch userPreference={theme}/>
+                </div>
+            </div>
+            <Spacer size="3xs"/>
+        </Document>
+    );
 }
 
 export default function AppWithProviders() {
-  const data = useLoaderData<typeof loader>();
+    const data = useLoaderData<typeof loader>();
 
-  return (
-    <AuthenticityTokenProvider token={data.csrfToken}>
-      <HoneypotProvider {...data.honeyProps}>
-        <App />
-      </HoneypotProvider>
-    </AuthenticityTokenProvider>
-  );
+    return (
+        <HoneypotProvider {...data.honeyProps}>
+            <AuthenticityTokenProvider token={data.csrfToken}>
+                <App/>
+            </AuthenticityTokenProvider>
+        </HoneypotProvider>
+    );
+}
+
+function ThemeSwitch({userPreference}: { userPreference?: Theme }) {
+    const fetcher = useFetcher<typeof action>()
+
+    const [form] = useForm({
+        id: 'theme-switch',
+        lastSubmission: fetcher.data?.submission,
+        onValidate({formData}) {
+            return parse(formData, {schema: ThemeFormSchema})
+        },
+    })
+
+    const mode = userPreference ?? 'light'
+    const nextMode = mode === 'light' ? 'dark' : 'light'
+    const modeLabel = {
+        light: (
+            <Icon name="sun">
+                <span className="sr-only">Light</span>
+            </Icon>
+        ),
+        dark: (
+            <Icon name="moon">
+                <span className="sr-only">Dark</span>
+            </Icon>
+        ),
+    }
+
+    return (
+        <fetcher.Form method="POST" {...form.props}>
+            <input type="hidden" name="theme" value={nextMode}/>
+            <div className="flex gap-2">
+                <button
+                    name="intent"
+                    value="update-theme"
+                    type="submit"
+                    className="flex h-8 w-8 cursor-pointer items-center justify-center"
+                >
+                    {modeLabel[mode]}
+                </button>
+            </div>
+            <ErrorList errors={form.errors} id={form.errorId}/>
+        </fetcher.Form>
+    )
 }
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Epic Tech" },
-    {
-      name: "Cool Tech Website!",
-      content: "An application where cool tech lives",
-    },
-  ];
+    return [
+        {title: "Epic Tech"},
+        {
+            name: "Cool Tech Website!",
+            content: "An application where cool tech lives",
+        },
+    ];
 };
 
 export function ErrorBoundary() {
-  return (
-    <Document>
-      <GeneralErrorBoundary />
-    </Document>
-  );
+    return (
+        <Document>
+            <div className="flex-1">
+                <GeneralErrorBoundary/>
+            </div>
+        </Document>
+    );
 }
