52ed84458ba330b9c346a7cd0403403bdefda5c4
Justin Andrews <justin.48@hotmail.com>
2024-02-18 12:21:24 -0700

    took out split

--- app/utils/toast.server.ts
+++ app/utils/toast.server.ts
@@ -20,7 +20,7 @@ export const toastSessionStorage = createCookieSessionStorage({
     sameSite: "lax",
     path: "/",
     httpOnly: true,
-    secrets: process.env.SESSION_SECRET.split(","),
+    // secrets: process.env.SESSION_SECRET.split(","),
     secure: process.env.NODE_ENV === "production",
   },
 });
